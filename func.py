43f568228ea9f5a397b107d8607bacd4925f597f
Bob Kozdemba <bkozdemba@gmail.com>
2021-07-15 17:11:37 -0500

    updated

--- func.py
+++ func.py
@@ -7,74 +7,48 @@ import numpy as np
 model = None
 
 def main(context: Context):
-    """ 
-    Serverless Function 
-    The context parameter contains the Flask request object and any
+    """    Serverless Function 
+    Args:
+        context (Context): The context parameter contains the Flask request object and any
     CloudEvent received with the request.
+
+    Returns:
+        [tuple]: (prediction dict, http status)
     """
-    r = context.request
-    
     #
-    # Debug output.
-    #
-    logging.warning(f'')
-    logging.warning(f'')
-    logging.warning(f'**************************************************************')
-    logging.warning(f'************** main() called.')
-    logging.warning(f'**************  context.request: {r}')
-    logging.warning(f'**************  content_length: {r.content_length}')
-    logging.warning(f'**************  get_data: {r.get_data()}')
-    logging.warning(f'**************  type: {type(r.get_data())}')
-    logging.warning(f'**************  headers: {r.headers}')
-
-    #
-    # Get the json data from Flask and convert it a python dictionary.
-    #
-    data = json.loads(r.get_data())
-    logging.warning(f'**************  data: {data}')
-    logging.warning(f'**************  type(data): {type(data)}')
-
-    #
-    # Load the model from storage.
+    # Load the model from storage. Globals are used to prevent the model
+    # from being loaded more than once.
     #
     global model
     
     if model == None:
-        logging.warning(f'************** Loading model.')
+        logging.info(f'Loading model.')
         try:
             model = pickle.load(open("iris_rfc.pkl", "rb"))
-            logging.warning(f'************** Iris model loaded.')
-
+            logging.info(f'Iris model loaded.')
         except Exception as e:
-            logging.warning(f'************** Load model failed!!!')
+            logging.warning(f'Load model failed!!!')
             model = None
-
     #
-    # Prepare the data for use in a prediction.
+    # Get the json data from Flask and convert it to a python dictionary.
     #
-
-    #
-    # Get the 4 parameters from the dictionary.
-    #
-    predict_request = [data['sl'],data['sw'],data['pl'], data['pw']] 
+    r = context.request
+    data = json.loads(r.get_data())
     
     #
-    # Convert the json to an numpy array
+    # Prepare the data for use in a prediction. Get the 4 prediction parameters from the dictionary
+    # and convert them to a flattened numpy array. The format is often model dependent.
     #
-    predict_request = np.array(predict_request).reshape(1,-1)
+    predict_request = np.array([data['sl'],data['sw'],data['pl'], data['pw']]).reshape(1,-1)
     
     #
-    # Call the model's predict function by passing in the numpy array.
+    # Call the model's predict function by passing in the numpy array and 
+    # return the prediction and an OK status.
     #
-    y_hat = model.predict(predict_request)
-    
-    #
-    # Return the prediction
-    #
-    output = {'y_hat': int(y_hat[0])}
-    logging.warning(f'************** prediction: {output}')
-    logging.warning(f'**************************************************************')
-    logging.warning(f'')
-    logging.warning(f'')
-   
-    return { "prediction": output['y_hat'] }, 200
+    try:
+        y_hat = model.predict(predict_request)
+        output = {'y_hat': int(y_hat[0])}
+        return { "prediction": output['y_hat'] }, 200
+    except:
+        logging.warning(f'Bad data or prediction')
+        return { "no": "content" }, 204
