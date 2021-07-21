66c590efa9535dfbe08289db74ea1a9f230474ab
Bob Kozdemba <bkozdemba@gmail.com>
2021-07-21 11:27:03 -0500

    updated

--- 01-iris-rest-curl.sh
+++ 01-iris-rest-curl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-curl -X POST -H "Content-Type: application/json" --data '{"sl": 5.9, "sw": 3.0, "pl": 5.1, "pw": 1.8}' http://model-server-functions.apps.shared-na46.openshift.opentlc.com
+URL=http://model-server-model-server.apps.ocp.3f4e.sandbox1385.opentlc.com
+curl -X POST -H "Content-Type: application/json" --data '{"sl": 5.9, "sw": 3.0, "pl": 5.1, "pw": 1.8}' $URL
 
 
